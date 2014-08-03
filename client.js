9e8e0da3b0397d175c8b4dfd3b733f9278561c66
Nexus Unplugged <DanH1420@gmail.com>
2014-08-03 18:50:09 -0500

    Initial commit

---
+++ client.js
@@ -0,0 +1,116 @@
+var request = require('request');
+var im = require('imagemagick');
+var sleep = require('sleep');
+var fs = require('fs');
+
+var config = JSON.parse(fs.readFileSync("config.json"));
+
+function makeRequest(){
+	request(config.poll_url + config.id, handleResponse);
+}
+
+function handleResponse(err, res, body){
+	if(!err && res.statusCode === 200){
+		try{
+			var data = JSON.parse(body);
+			for(var i = 0; i < data.length; i++)
+				handleInput(data[i]);
+		}catch(e){
+			handleError(e, 200);
+		}
+	}else
+		handleError(err, res.statusCode);
+	makeRequest();
+}
+
+function handleError(err, status){
+	console.log("ERROR", res.statusCode, err);
+}
+
+function handleInput(data){
+	console.log("DATA", data);
+	if(data.type){
+		switch(data.type){
+			case "action":
+				doAction(data.path, data.p);
+				break;
+			case "capture":
+				updateImage();
+				break;
+			default:
+				console.log("Unknown type: " + data.type);
+		}
+	}
+}
+
+function doAction(path, p, callback){
+	var url = config.camera_url + path + "?t=" + config.password;
+	if(p && p.length)
+		url += "&p=%" + p;
+	request(url, function(err, res, body){
+		if(callback)
+			callback(!err && res.statusCode === 200);
+		else if(err || res.statusCode !== 200))
+			console.log("Error sending command to GoPro:", path, p);
+	});
+}
+
+function updateImage(){
+	console.log("Taking picture...");
+	doAction("SH", "01");
+	sleep.sleep(1);
+	doAction("SH", "00");
+
+	console.log("Getting image list from camera...");
+	request(config.dcim_url + "101GOPRO/", function(err, res, body){
+		if(!err && res.statusCode === 200)
+			var temp = body.split('.JPG</a>');
+			if(temp.length > 1){
+				var temp = temp[temp.length - 2].split('>');
+				var latest = config.dcim_url + "101GOPRO/" + temp[temp.length - 1] + ".JPG";
+
+				console.log("Downloading image from camera...");
+				request(latest, function(err, res, body){
+					if(!err && res.statusCode === 200){
+						console.log("Resizing image...");
+						im.resize({
+							srcPath: "latest.jpg",
+							dstPath: "latest.jpg",
+							width: 1024,
+							height: 768
+						}, function(err){
+							if(!err){
+								console.log("Uploading image...");
+								fs.createReadStream("latest.jpg").pipe(request.post(config.update_url, function(err, res, body){
+									if(!err && res.statusCode === 200){
+										console.log("Wiping memory...");
+										doAction("DA", function(success){
+											if(success)
+												imageComplete();
+											else
+												imageComplete("Couldn't wipe SD card.");
+										});
+									}else
+										imageComplete("Couldn't upload image to server.");
+								}));
+							}else
+								imageComplete("Couldn't resize image.");
+						});
+					}else
+						imageComplete("Couldn't download image from camera.");
+				}).pipe(fs.createWriteStream("latest.jpg"));
+			}else
+				imageComplete("Couldn't find any images on the camera.");
+		}else
+			imageComplete("Couldn't load image list from camera.");
+	});
+}
+
+function imageComplete(err){
+	if(err)
+		console.log(err);
+	else
+		console.log("Done!");
+}
+
+makeRequest();
---
+++ server.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var fs = require('fs');
+
+app.use(express.static('static'));
+server.listen(8080);
+
+io.enable('browser client minification');
+io.enable('browser client etag');
+io.enable('browser client gzip');
+io.set('log level', 1);
+io.set('transports', ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']);
+
+var reqs = {};
+var queue = {};
+
+function sendRes(id, data){
+	if(queue[id]){
+		if(data)
+			queue[id].push(data);
+	}else if(data !== undefined)
+		queue[id] = [data];
+	else
+		queue[id] = [];
+
+	if(reqs[id]){
+		reqs[id].res.send(queue[id]);
+		console.log("Sent " + queue[id].length + " message(s) to " + id);
+		delete reqs[id];
+		delete queue[id];
+	}
+}
+
+function checkReqs(){
+	for(var id in reqs){
+		if(+new Date - reqs[id].time > 30000){
+			sendRes(id);
+		}
+	}
+}
+
+app.get('/poll/:id', function(req, res){
+	reqs[req.params.id] = {
+		req: req,
+		res: res,
+		time: +new Date
+	};
+
+	if(queue[req.params.id])
+		sendRes(req.params.id);
+});
+
+app.post('/upload', function(req, res){
+	var upload = fs.createWriteStream("latest.jpg");
+	req.setEncoding('utf8');
+	req.on('data', function(chunk){
+		upload.write(chunk);
+	});
+	req.on('end', function(){
+		upload.end();
+		res.send("Got it!");
+	});
+});
+
+io.sockets.on('connection', function(socket){
+	socket.on('send', function(id, data){
+		if(data.substr(0, 1) === "{" || data.substr(0, 1) === "["){
+			try{
+				var parsedData = JSON.parse(data);
+				sendRes(id, parsedData);
+			}catch(e){
+				sendRes(id, data);
+			}
+		}else
+			sendRes(id, data);
+	});
+});
+
+setInterval(checkReqs, 5000);
+console.log("Started.");
